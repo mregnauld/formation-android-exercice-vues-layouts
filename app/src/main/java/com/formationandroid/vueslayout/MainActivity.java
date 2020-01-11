772ea68495f818e4fdbb07ebb2b0a18d4089b465
mregnauld <mregnauld@gmail.com>
2020-01-11 19:15:47 +0100

    Mise a jour Android 10

--- app/src/main/java/com/formationandroid/vueslayout/MainActivity.java
+++ app/src/main/java/com/formationandroid/vueslayout/MainActivity.java
@@ -1,10 +1,11 @@
 package com.formationandroid.vueslayout;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ScrollView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
